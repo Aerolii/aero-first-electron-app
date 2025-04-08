668ab36579be8f625dcb2a324b932c0706f0d9fd
张晓波 <xiaobo.z@kusaauto.com>
2025-04-08 14:41:19 +0800

    feat: 引入 renderer 脚本

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 import { BrowserWindow, app } from 'electron'
 import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 console.log('Electron app starting...')
 
